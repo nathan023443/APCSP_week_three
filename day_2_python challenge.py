cbebb301a98f3301ec3f43b8810f959663a14b83
Denis <ndenis@cps.k12.il.us>
2024-09-13 12:17:07 -0500

    made a calc for salesmen

---
+++ day_2_python challenge.py
@@ -0,0 +1,12 @@
+# this is just for testing for collabrative work
+# create an input for the user to input their age 
+# create an input for the user to input their favo
+
+employe_name = input("What is your name? ")
+sales = float(input("How much did you sell? "))
+ammount_made = float (sales * .13)
+
+print(employe_name+ " , you are entilted to $ " +  str(ammount_made))
+
+
+
